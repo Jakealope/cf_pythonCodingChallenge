ea083f5bcf54756676ab8cba6ab148be84ab8271
Jakealope <Jakealope@gmail.com>
2015-01-03 18:36:16 -0800

    Added the html and js files for the todo web app

---
+++ Todo.js
@@ -0,0 +1,25 @@
+var inItemText = document.getElementById("inItemText");
+inItemText.focus();
+var btnNew = document.getElementById("btnAdd");
+
+function addNewItem(list, itemText) {
+	var listItem = document.createElement("li");
+	listItem.innerText = itemText;
+	list.appendChild(listItem);
+	list.insertBefore(listItem, list.childNodes[0]);
+	listItem.onclick = function () {
+		this.parentNode.removeChild(this);
+	}
+}
+
+btnNew.onclick = function () {
+	var itemText = inItemText.value;
+	if (!itemText || itemText === "" || itemText === " ") {
+		return false;
+	}
+
+	addNewItem(document.getElementById("todoList"), itemText);
+
+	inItemText.focus();
+	inItemText.select();
+}
