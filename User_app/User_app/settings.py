6063030571580ec2b33cc1339bcb5b1b961cdfa5
Jakealope <Jakealope@gmail.com>
2015-01-13 09:14:46 -0800

    added db settings for Heroku

--- User_app/User_app/settings.py
+++ User_app/User_app/settings.py
@@ -78,12 +78,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-
-
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
@@ -92,3 +87,13 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
--- User_app/User_app/wsgi.py
+++ User_app/User_app/wsgi.py
@@ -7,8 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "User_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UserApp.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
